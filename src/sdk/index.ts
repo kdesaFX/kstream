9f8f19b976ce03550a24267089f46a717c980a13
xanning <sptrspostok@gmail.com>
2026-07-20 20:17:23 +0300

    RE ENABLE SDK

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -2,5 +2,5 @@
  * COMMENT export * from "./mock"; IF running in prod
  * COMMENT export * from "@p-stream/providers"; IF running locally
  */
-// export * from "@p-stream/providers";
-export * from "./mock";
+export * from "@p-stream/providers";
+// export * from "./mock";
